a91daa0d5cdff7440d8a7049e6e3c4109d026068
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2021-02-28 23:38:02 -0600

    Added email functionality to the contact form

--- Day 60 Blog Project + Form/main.py
+++ Day 60 Blog Project + Form/main.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+my_gmail = os.getenv("my_gmail")
+gmail_password = os.getenv("gmail_password")
 
 blog_json_url = "https://api.npoint.io/43644ec4f0013682fc0d"
 response = requests.get(url=blog_json_url)
@@ -24,9 +32,29 @@ def about_page():
     return render_template("about.html")
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact_page():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        send_email(data)
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(data):
+    message = f"Subject:New Message From Your Website\n\nName: {data['name']}\nEmail: {data['email']}\nPhone: {data['phone']}\nMessage: {data['message']}"
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_gmail, password=gmail_password)
+        connection.sendmail(
+            from_addr=my_gmail,
+            to_addrs=my_gmail,
+            msg=message
+        )
 
 
 if __name__ == "__main__":
