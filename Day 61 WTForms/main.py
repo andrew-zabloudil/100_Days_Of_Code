33a4e36055d22f2825232d2dd2c18765a1b6a72c
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2021-03-01 16:41:51 -0600

    Added validation to form

--- Day 61 WTForms/main.py
+++ Day 61 WTForms/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
 from dotenv import load_dotenv
 import os
 
@@ -12,8 +12,9 @@ app.secret_key = os.getenv("WTFORMS_SECRET_KEY")
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[Email()])
     password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Login')
 
 
 @app.route("/")
@@ -21,9 +22,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 
