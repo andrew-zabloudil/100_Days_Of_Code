ba99c697b2c5d196a75e34f3cc0f892b643036c1
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2021-03-02 12:28:53 -0600

    Change html to use base template

--- Day 61 WTForms/main.py
+++ Day 61 WTForms/main.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = os.getenv("WTFORMS_SECRET_KEY")
 
 
@@ -25,7 +26,11 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     return render_template('login.html', form=login_form)
 
 
