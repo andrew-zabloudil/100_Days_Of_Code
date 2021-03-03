31faf98b667336bd74b50b291fc5599c7a97526c
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2021-03-03 15:54:46 -0600

    added sqlite, now renders pages and receives form

--- Day 63 Virtual Bookshelf with SQLite/main.py
+++ Day 63 Virtual Bookshelf with SQLite/main.py
@@ -1,20 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
+db = sqlite3.connect(
+    "Day 63 Virtual Bookshelf with SQLite/books-collection.db")
 
 all_books = []
 
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html", all_books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        data = request.form.to_dict()
+        all_books.append(data)
+        print(all_books)
+        return redirect(url_for('home'))
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
