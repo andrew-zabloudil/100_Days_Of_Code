f977eca2c54e1a60d29ff128081ec541efdef474
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2021-03-06 15:08:20 -0600

    Added random route that returns json

--- Day 66 Cafe REST API/main.py
+++ Day 66 Cafe REST API/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 app = Flask(__name__)
 
@@ -30,8 +31,23 @@ def home():
 
 
 @app.route("/random")
-def random():
-    pass
+def get_random_cafe():
+    all_cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(all_cafes)
+    cafe_info = jsonify(cafe={
+        "id": random_cafe.id,
+        "name": random_cafe.name,
+        "map_url": random_cafe.map_url,
+        "img_url": random_cafe.img_url,
+        "location": random_cafe.location,
+        "seats": random_cafe.seats,
+        "has_toilet": random_cafe.has_toilet,
+        "has_wifi": random_cafe.has_wifi,
+        "has_sockets": random_cafe.has_sockets,
+        "can_take_calls": random_cafe.can_take_calls,
+        "coffee_price": random_cafe.coffee_price,
+    })
+    return cafe_info
 
 # HTTP GET - Read Record
 
