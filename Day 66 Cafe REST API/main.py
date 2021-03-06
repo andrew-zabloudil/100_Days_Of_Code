4b5b16540bf593c813eb8573b01133bb106da098
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2021-03-06 16:44:50 -0600

    Added random, all, search, add, update, and delete

--- Day 66 Cafe REST API/main.py
+++ Day 66 Cafe REST API/main.py
@@ -9,8 +9,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+API_KEY = "TopSecretAPIKey"
 
 # Cafe TABLE Configuration
+
+
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -24,6 +27,9 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 @app.route("/")
 def home():
@@ -34,20 +40,69 @@ def home():
 def get_random_cafe():
     all_cafes = db.session.query(Cafe).all()
     random_cafe = random.choice(all_cafes)
-    cafe_info = jsonify(cafe={
-        "id": random_cafe.id,
-        "name": random_cafe.name,
-        "map_url": random_cafe.map_url,
-        "img_url": random_cafe.img_url,
-        "location": random_cafe.location,
-        "seats": random_cafe.seats,
-        "has_toilet": random_cafe.has_toilet,
-        "has_wifi": random_cafe.has_wifi,
-        "has_sockets": random_cafe.has_sockets,
-        "can_take_calls": random_cafe.can_take_calls,
-        "coffee_price": random_cafe.coffee_price,
-    })
-    return cafe_info
+    return jsonify(cafe=random_cafe.to_dict())
+
+
+@app.route("/all")
+def get_all_cafes():
+    cafes = db.session.query(Cafe).all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
+
+
+@app.route("/search")
+def get_cafe_at_location():
+    query_location = request.args.get("loc")
+    cafe = db.session.query(Cafe).filter_by(location=query_location).first()
+    if cafe:
+        return jsonify(cafe=cafe.to_dict())
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+
+
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"Success": "Successfully added the new cafe."})
+
+
+@app.route("/update-price/<cafe_id>", methods=["PATCH"])
+def patch_cafe(cafe_id):
+    new_price = request.args.get("new_price")
+    cafe = db.session.query(Cafe).get(cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"Success": "Successfully updated the price."})
+    else:
+        return jsonify(error={"Not Found": "Sorry, a cafe with that id was not found."})
+
+
+@app.route("/report-closed/<cafe_id>", methods=["DELETE"])
+def report_closed(cafe_id):
+    if request.args.get("api-key") != API_KEY:
+        return jsonify(error={"error": "Sorry, that's not allowed. Make sure you have the correct API key."}), 403
+    else:
+        cafe = db.session.query(Cafe).get(cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"Success": "Successfully deleted the cafe."}), 200
+        else:
+            return jsonify(error={"Not Found": "Sorry, a cafe with that id was not found."}), 404
+
 
 # HTTP GET - Read Record
 
@@ -56,7 +111,6 @@ def get_random_cafe():
 # HTTP PUT/PATCH - Update Record
 
 # HTTP DELETE - Delete Record
-
 
 if __name__ == '__main__':
     app.run(debug=True)
