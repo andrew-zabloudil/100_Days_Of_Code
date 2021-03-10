6420f433be3a533aecf861a0c3367d8c45a04894
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2021-03-09 21:28:55 -0600

    Add comment form

--- Day 69 Adding Users/forms.py
+++ Day 69 Adding Users/forms.py
@@ -25,3 +25,8 @@ class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Let Me In!")
+
+
+class CommentForm(FlaskForm):
+    comment = CKEditorField("Comment", validators=[DataRequired()])
+    submit = SubmitField("Submit Comment")
